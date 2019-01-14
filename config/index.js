4e3354d5dff1a4f1c9663e951d9807d1d89bfa75
Jayshi <240638896@qq.com>
2019-01-14 18:04:49 +0800

    1.14

--- config/index.js
+++ config/index.js
@@ -11,13 +11,36 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
- /*     "/api":{
-        target: 'qlzm.com',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/'
-        }
-      }*/
+      /*     "/api":{
+             target: 'qlzm.com',
+             changeOrigin: true,
+             pathRewrite: {
+               '^/api': '/'
+             }
+           }*/
+    },
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map',
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    cssSourceMap: true
   },
 
   build: {
